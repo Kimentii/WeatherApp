e3d7a59b2e10ae822f09199a9264aff8150d27fa
Kimentii <malafeikirill@mail.ru>
2018-04-10 22:23:11 +0300

    Added getting location.

--- app/src/main/java/com/kimentii/weatherapp/MainActivity.java
+++ app/src/main/java/com/kimentii/weatherapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.kimentii.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static int PERMISSIONS_REQUEST_LOCATION = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +35,108 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION},
+                            PERMISSIONS_REQUEST_LOCATION);
+                    return;
+                } else {
+                    Log.d(TAG, "Has location permission.");
+                    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.0f, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            Log.d(TAG, "onLocationChanged");
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                            Log.d(TAG, "onStatusChanged");
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                            Log.d(TAG, "onProviderEnabled");
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            Log.d(TAG, "onProviderDisabled");
+                        }
+                    });
+                    List<String> providers = lm.getProviders(true);
+                    Log.d(TAG, "Num of providers: " + providers.size());
+                    Location bestLocation = null;
+                    for (String provider : providers) {
+                        Location l = lm.getLastKnownLocation(provider);
+                        if (l == null) {
+                            continue;
+                        }
+                        if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                            bestLocation = l;
+                        }
+                    }
+                    double longitude = bestLocation.getLongitude();
+                    double latitude = bestLocation.getLatitude();
+                    Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = null;
+                    try {
+                        addresses = gcd.getFromLocation(latitude, longitude, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (addresses.size() > 0) {
+                        Log.d(TAG, "Location: " + addresses.get(0).getLocality());
+                    } else {
+                        // do your stuff
+                    }
+                    Snackbar.make(view, "Done", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    Log.d(TAG, "Location permission was given.");
+                    LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    List<String> providers = lm.getProviders(true);
+                    Log.d(TAG, "Num of providers: " + providers.size());
+                    Location bestLocation = null;
+                    for (String provider : providers) {
+                        Location l = lm.getLastKnownLocation(provider);
+                        if (l == null) {
+                            continue;
+                        }
+                        if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                            // Found best last known location: %s", l);
+                            bestLocation = l;
+                        }
+                    }
+                    double longitude = bestLocation.getLongitude();
+                    double latitude = bestLocation.getLatitude();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 }
